d1bf27179f10b875195d32f685bf799d3f2b5663
wellifabio <wellifabio@hotmail.com>
2025-10-15 11:02:29 -0300

    re seed implementado

--- src/controllers/seed.js
+++ src/controllers/seed.js
@@ -1,87 +1,74 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const fs = require('fs');
-const path = require('path');
 
 async function inserePerfis() {
-    const perfisData = fs.readFileSync(path.join(__dirname, '../../data/perfis.csv'), 'utf8');
-    const linhas = perfisData.split('\r\n');
-    const resultados = await Promise.all(linhas.map((linha, i) => {
-        if (i > 0 && linha) {
-            const [id, perfil] = linha.split(';');
-            return prisma.perfil.create({
-                data: {
-                    id: Number(id),
-                    perfil
-                }
-            });
-        }
-        return null;
+    const perfisData = [
+        { "id": 1, "perfil": "Comum" },
+        { "id": 2, "perfil": "Administrador" },
+        { "id": 3, "perfil": "Tecnico" },
+        { "id": 4, "perfil": "Gerente" }
+    ];
+    const resultados = await Promise.all(perfisData.map((p) => {
+        return prisma.perfil.create({ data: p });
     }));
     return resultados.filter(s => s !== null && s !== undefined).length;
 }
 
 async function insereUsuarios() {
-    const usuariosData = fs.readFileSync(path.join(__dirname, '../../data/usuarios.csv'), 'utf8');
-    const linhas = usuariosData.split('\r\n');
-    const resultados = await Promise.all(linhas.map((linha, i) => {
-        if (i > 0 && linha) {
-            const [id, senha, perfil] = linha.split(';');
-            return prisma.usuario.create({
-                data: {
-                    id: Number(id),
-                    senha,
-                    perfil: Number(perfil)
-                }
-            });
-        }
-        return null;
+    const usuariosData = [
+        { "id": 1, "senha": "111111", "perfil": 1 },
+        { "id": 2, "senha": "212121", "perfil": 2 },
+        { "id": 3, "senha": "414141", "perfil": 4 },
+        { "id": 4, "senha": "313131", "perfil": 3 }
+    ]
+    const resultados = await Promise.all(usuariosData.map((u) => {
+        return prisma.usuario.create({ data: u });
     }));
     return resultados.filter(s => s !== null && s !== undefined).length;
 }
 
-async function insereEquipamentos() {
-    const equipamentosData = fs.readFileSync(path.join(__dirname, '../../data/equipamentos.csv'), 'utf8');
-    const linhas = equipamentosData.split('\r\n');
-    const resultados = await Promise.all(linhas.map((linha, i) => {
-        if (i > 0 && linha) {
-            const [id, equipamento, imagem, descricao, ativo, data] = linha.split(';');
-            return prisma.equipamento.create({
-                data: {
-                    id: Number(id),
-                    equipamento,
-                    imagem,
-                    descricao,
-                    ativo: Number(ativo),
-                    data: new Date(data)
-                }
-            });
-        }
-        return null;
-    }));
-    return resultados.filter(s => s !== null && s !== undefined).length;
-}
+// async function insereEquipamentos() {
+//     const equipamentosData = fs.readFileSync(path.join(__dirname, '../../data/equipamentos.csv'), 'utf8');
+//     const linhas = equipamentosData.split('\r\n');
+//     const resultados = await Promise.all(linhas.map((linha, i) => {
+//         if (i > 0 && linha) {
+//             const [id, equipamento, imagem, descricao, ativo, data] = linha.split(';');
+//             return prisma.equipamento.create({
+//                 data: {
+//                     id: Number(id),
+//                     equipamento,
+//                     imagem,
+//                     descricao,
+//                     ativo: Number(ativo),
+//                     data: new Date(data)
+//                 }
+//             });
+//         }
+//         return null;
+//     }));
+//     return resultados.filter(s => s !== null && s !== undefined).length;
+// }
 
-async function insereComentarios() {
-    const comentariosData = fs.readFileSync(path.join(__dirname, '../../data/comentarios.csv'), 'utf8');
-    const linhas = comentariosData.split('\r\n');
-    const resultados = await Promise.all(linhas.map((linha, i) => {
-        if (i > 0 && linha) {
-            const [id, comentario, equipamento, perfil, data] = linha.split(';');
-            return prisma.comentario.create({
-                data: {
-                    id: Number(id),
-                    comentario,
-                    equipamento: Number(equipamento),
-                    perfil: Number(perfil),
-                    data: new Date(data)
-                }
-            });
-        }
-        return null;
-    }));
-    return resultados.filter(s => s !== null && s !== undefined).length;
-}
+// async function insereComentarios() {
+//     const comentariosData = fs.readFileSync(path.join(__dirname, '../../data/comentarios.csv'), 'utf8');
+//     const linhas = comentariosData.split('\r\n');
+//     const resultados = await Promise.all(linhas.map((linha, i) => {
+//         if (i > 0 && linha) {
+//             const [id, comentario, equipamento, perfil, data] = linha.split(';');
+//             return prisma.comentario.create({
+//                 data: {
+//                     id: Number(id),
+//                     comentario,
+//                     equipamento: Number(equipamento),
+//                     perfil: Number(perfil),
+//                     data: new Date(data)
+//                 }
+//             });
+//         }
+//         return null;
+//     }));
+//     return resultados.filter(s => s !== null && s !== undefined).length;
+// }
 
 // Função principal para semear os dados
 async function run(req, res) {
@@ -89,12 +76,12 @@ async function run(req, res) {
         await prisma.$connect();
         // await prisma.comentario.deleteMany();
         // await prisma.equipamento.deleteMany();
-        // await prisma.usuario.deleteMany();
-        // await prisma.perfil.deleteMany();
+        await prisma.usuario.deleteMany();
+        await prisma.perfil.deleteMany();
         const perfis = await inserePerfis();
         const usuarios = await insereUsuarios();
-        const equipamentos = await insereEquipamentos();
-        const comentarios = await insereComentarios();
+        // const equipamentos = await insereEquipamentos();
+        // const comentarios = await insereComentarios();
         await prisma.$disconnect();
         res.json({ perfis, usuarios, equipamentos, comentarios });
     } catch (error) {
